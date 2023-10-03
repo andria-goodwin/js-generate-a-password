1f3a87874ac447b24b8ce2ddbf50059e05235fc1
andria-goodwin <143760838+andria-goodwin@users.noreply.github.com>
2023-10-03 17:57:01 -0500

    added refresh message and fixed bug

--- develop/script.js
+++ develop/script.js
@@ -8,73 +8,118 @@ numbersChar: "0123456789",
 symbolsChar: "*&^%$#@!?><{}"
 }
 
-var inputs = [""];
-var randomPassword = "";
+var inputs = [];
+var randomPassword = [];
 
 // Define the generatePassword function
 function generatePassword() {
-
-// 1. Prompt the user for password criteria
+// Prompt the user for password criteria
     var passwordLength = prompt ("Enter a number between 8 characters and 128 characters to be the password length and click 'OK'.");
 
-//    a. password length between 8 - 128 characters
-//        i. validate that input is between 8 an 128 chars
+//  password length between 8 - 128 characters
+//  validate that input is between 8 an 128 chars
     if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
         alert("You must choose between 8 and 128 characters. Please try again.");
         return;
     }
 
-      //Changes passwordLength string to integer
+//Changes passwordLength string to integer
     const confirmLength = parseInt(passwordLength)
 
-//    b. password contains upper case, lower case, numbers, and/or symbols
-//        i. validate that at least 1 option has been selected
-    var upperCase = confirm("Click 'OK' to include upper case letters or click 'Cancel' for no upper case letters");
+// password contains upper case, lower case, numbers, and/or symbols
+// validate that at least 1 option has been selected
+    var upperCase = confirm("Click 'OK' to include UPPER CASE letters or click 'Cancel' for no upper case letters");
 
-    var lowerCase = confirm("Click 'OK' to include lower case letters or click 'Cancel' for no lower case letters.");
+    var lowerCase = confirm("Click 'OK' to include LOWER CASE letters or click 'Cancel' for no lower case letters.");
 
-    var numbers = confirm("Click 'OK' to include numbers letters or click 'Cancel' for no numbers.");
+    var symbols = confirm("Click 'OK' to include SYMBOLS or click 'Cancel' for no symbols.");
 
-    var symbols = confirm("Click 'OK' to include symbols or click 'Cancel' for no symbols.");
+    var numbers = confirm("Click 'OK' to include NUMBERS or click 'Cancel' for no numbers.");
 
-    if (upperCase === false && lowerCase === false && numbers === false && symbols === false) {
+    if (upperCase === false && lowerCase === false && symbols === false && numbers === false) {
         alert("You must choose at least one character type.");
         return;
     }
 
-  // 2. Generate a password that matches input criteria
+// [upperCase, lowerCase, symbols, numbers]
+    var arrayCriteria = [];
+
+// Generate a password that matches input criteria
     if (upperCase === true) {
         for (var i = 0; i < chars.upperCaseChar.length; i++) {
       inputs.push(chars.upperCaseChar[i]);
         }
+        arrayCriteria.push(true);
+    } else {
+        arrayCriteria.push(false);
     }
 
     if (lowerCase === true) {
       for (var i = 0; i < chars.lowerCaseChar.length; i++) {
         inputs.push(chars.lowerCaseChar[i]);
       }
+      arrayCriteria.push(true);
+    } else {
+        arrayCriteria.push(false);
     }
 
     if (symbols === true) {
       for (var i = 0; i < chars.symbolsChar.length; i++) {
         inputs.push(chars.symbolsChar[i]);
       }
+      arrayCriteria.push(true);
+    } else {
+        arrayCriteria.push(false);
     }
 
     if (numbers === true) {
       for (var i = 0; i < chars.numbersChar.length; i++) {
         inputs.push(chars.numbersChar[i]);
       }
+      arrayCriteria.push(true);
+    } else {
+        arrayCriteria.push(false);
     }
 
-    for (var i = 0; i < confirmLength; i++) {
+    // Ensures there will be at least 1 of each character type included if selected
+    var testPassword = [];
+
+    for (var i = 0; i < arrayCriteria.length; i++) {
+      // [upperCase, lowerCase, symbols, numbers]
+      if (i === 0) {
+        if (arrayCriteria[0]) { 
+          testPassword += chars.upperCaseChar[Math.floor(Math.random() * chars.upperCaseChar.length)]
+        }
+      }
+      if (i === 1) {
+        if (arrayCriteria[1]) {
+          testPassword += chars.lowerCaseChar[Math.floor(Math.random() * chars.lowerCaseChar.length)]
+        }
+      }
+      if (i === 2) {
+        if (arrayCriteria[2]) {
+          testPassword += chars.numbersChar[Math.floor(Math.random() * chars.numbersChar.length)]
+        }
+      }
+      if (i === 3) {
+        if (arrayCriteria[3]) {
+          testPassword += chars.symbolsChar[Math.floor(Math.random() * chars.symbolsChar.length)]
+        }
+      }
+    }
+    
+    // Adds however many random places are needed in front of arrayCriteria to fulfill the desired password length
+    for (var i = 0; i < confirmLength - testPassword.length; i++) {
       inputs[Math.floor(Math.random() * inputs.length)];
   
       randomPassword += inputs[Math.floor(Math.random() * inputs.length)];
     }
 
-  // 3. Display generated password to the page
-    return randomPassword;
+    var finalPassword = randomPassword.concat(testPassword);
+
+// Display generated password to the page
+    alert("Your new password has been generated! Click 'OK' to see your new password. If you'd like a different password, refresh the page and then try again");
+    return finalPassword;
 }
 
 // Write password to the #password input
@@ -84,7 +129,6 @@ function writePassword() {
 
 // This writes the password to page
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
